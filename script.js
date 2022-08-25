5ad8ee8e52522697c7d53058395179e8b6cad2e6
Gar-Yin Lam <garyin.lam@gmail.com>
2022-08-25 17:13:58 +0100

    ai object

--- script.js
+++ script.js
@@ -33,7 +33,48 @@ const player = {
   }]
 };
 
+const ai = {
+  grid: Array(10).fill(Array(10).fill(false)),
+  ships: [{
+    name: "carrier",
+    size: 5,
+    coordinates: [0,0],
+    orientation: "ns",
+    isDestroyed: false
+  },{
+    name: "battleship",
+    size: 4,
+    coordinates: [0,0],
+    orientation: "ns",
+    isDestroyed: false
+  },{
+    name: "destroyer",
+    size: 3,
+    coordinates: [0,0],
+    orientation: "ns",
+    isDestroyed: false
+  },{
+    name: "submarine",
+    size: 3,
+    coordinates: [0,0],
+    orientation: "ns",
+    isDestroyed: false
+  },{
+    name: "patrol",
+    size: 2,
+    coordinates: [0,0],
+    orientation: "ns",
+    isDestroyed: false
+  }]
+};
+
 const grids = document.getElementsByClassName("grid");
+const startButton = document.getElementById("start");
+const placeButton = document.getElementById("place-ship");
+const rotateButton = document.getElementById("rotate");
+
+
+
 
 const letters = [' ','A','B','C','D','E','F','G','H','I','J'];
 for (const item of grids) {
@@ -44,9 +85,51 @@ for (const item of grids) {
   }
 }
 
-const boxes = document.getElementsByClassName("grid__box");
-for (const box of boxes) {
-  box.coord = [letters.indexOf(box.innerHTML.slice(0,1)),Number(box.innerHTML.slice(1))];
-  box.addEventListener("click",() => console.log(box.coord));
+let lastClicked = [0,0];
+
+
+
+
+const isGameStarted = false;
+
+const startGame = () => {
+  //start game
+  setupGame()
 }
 
+const setupGame = () => {
+  //game setup
+  placeButton.classList.add("show");
+  rotateButton.classList.add("show");
+  player.ships.forEach((ship) => {
+    grids[0].innerHTML += `<div class="ship ship__${ship.name}"></div>`;
+  });
+
+  setupBoxes();
+
+  
+}
+
+const setupBoxes = () => {
+  const boxes = document.getElementsByClassName("grid__box");
+  for (const box of boxes) {
+    box.coord = [letters.indexOf(box.innerHTML.slice(0,1)),Number(box.innerHTML.slice(1))];
+    const styleText = `
+    grid-row: ${box.coord[1]+1} ;
+    grid-column: ${box.coord[0]+1};
+    `;
+    box.style.cssText += styleText;
+    box.addEventListener("click",() => {
+      lastClicked = box.coord;
+      console.log(lastClicked);
+    });
+  }
+}
+
+const resetGame = () => {
+  //resets game
+}
+
+
+startButton.addEventListener("click", startGame);
+// placeButton.addEventListener("click", );
